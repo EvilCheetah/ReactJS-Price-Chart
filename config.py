daa2eae9d68fc033e2512cd8c81bc9c2a12b0877
Eugene M <eugene.moschyn@gmail.com>
2022-10-06 22:13:05 -0700

    add(): typechecking for optional parameter

--- config.py
+++ config.py
@@ -34,10 +34,15 @@ def _check_variable_types() -> None:
     if ( not getenv('MIN_NUMBER_OF_ENTRIES_PER_PLATFORM').isdigit() ):
         raise ValueError("'MIN_NUMBER_OF_ENTRIES_PER_PLATFORM' MUST be non-negative")
     
-    if ( not getenv('MAX_NUMBER_OF_ENTRIES_PER_PLATFORM').isdigit() ):
+    if (
+        _is_max_present() and
+        not getenv('MAX_NUMBER_OF_ENTRIES_PER_PLATFORM').isdigit()
+    ):
+        print(f':{getenv("MAX_NUMBER_OF_ENTRIES_PER_PLATFORM")}:')
         raise ValueError("'MAX_NUMBER_OF_ENTRIES_PER_PLATFORM' MUST be non-negative")
     
     if (
+        _is_max_present() and
         int(getenv('MIN_NUMBER_OF_ENTRIES_PER_PLATFORM')) > int(getenv('MAX_NUMBER_OF_ENTRIES_PER_PLATFORM'))
     ):
         raise ValueError(
@@ -45,7 +50,11 @@ def _check_variable_types() -> None:
             "MUST greater or equal(>=) to "
             "'MIN_NUMBER_OF_ENTRIES_PER_PLATFORM'"
         )
-    
+
+
+def _is_max_present() -> bool:
+    return ( getenv('MAX_NUMBER_OF_ENTRIES_PER_PLATFORM') )
+
 
 def _create_necessary_directories() -> None:
     '''
