570881dca7cfb3a41396c39ee40bd68917c901cf
Eugene M <eugene.moschyn@gmail.com>
2022-10-06 21:35:11 -0700

    fix(): add config into main to load .env

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from pprint import pprint
+from config import config
+from prices.generate_prices import generate_prices
+from prices.save import save_data
+
+
 def main():
-    pass
+    config()
+    data = generate_prices(20)
+    save_data(data)
 
 
 if __name__ == '__main__':
